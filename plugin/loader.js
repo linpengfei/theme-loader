c06291ac1804bfae1bf8ec84a17a9dde33041167
linpengfei <a569836198@vip.qq.com>
2020-04-02 23:26:28 +0800

    修改删除相关内容

--- plugin/loader.js
+++ plugin/loader.js
@@ -3,104 +3,109 @@ let loaderUtils = require('loader-utils');
 let validate = require('schema-utils');
 let path = require('path');
 const schema = require('./optionSchema.json');
+const syntax = require('postcss-less');
+const storage = require('./contentTemp');
+const stringify = require('./stringifier');
 let fs = require('fs');
-let valList = new Set();
-let themaFileInit = false;
 let globalVarsSet = new Set();
 function themeLoader(source) {
-    const option = loaderUtils.getOptions(this);
-    validate(option, schema);
-    this.options = option;
-    const { themeFile, globalVars } = option;
-    Object.keys(globalVars).forEach(key => {
-      globalVarsSet.add(key);
-    });
-    console.log('globalVarsSet:', globalVarsSet);
-    // 读取文件内容
-    // 样式文件未初始化，则先加载样式文件
-//     if(!themaFileInit) {
-//       const themeContent = fs.readFileSync(themeFile, { encoding: 'utf-8' }).toString();
-//       const themeRoot = postcss.parse(themeContent, { from: themeFile });
-//       themeRoot.walkAtRules( rule => {
-//         const { name } = rule;
-//         if (name !== "import") {
-//           valList.add(name.replace(/:$/, ""));
-//         }
-//         themaFileInit = true;
-//       });
-//     }
-// if(this.resourcePath === themeFile) {
-//   return source;
-// }
-    const root = postcss.parse(fs.readFileSync(this.resourcePath, { encoding: 'utf-8' }).toString(), { from: this.resourcePath });
-    let importTheme = false;
-    // root.walkAtRules('import', rule => {
-    //   const params = rule.params.replace(/'/g, '').toString();
-    //   if(/\.less$/.test(params) && path.resolve(this.context, params) === themeFile) {
-    //       // console.log('import theme file true');
-    //       importTheme = true;
-    //   }
-    // });
+  const option = loaderUtils.getOptions(this);
+  validate(option, schema);
+  this.options = option;
+  const { globalVars } = option;
+  console.log(globalVars);
+  Object.keys(globalVars).forEach(key => {
+    globalVarsSet.add(key);
+  });
+  const callback = this.async();
+  postcss().process(source, { syntax: syntax }).then(result => {
+    const root = result.root;
+    let sourceTemp = source;
     const atRuleName = new Set();
     if (globalVarsSet.size) {
       // 遍历相关节点,并移除无用节点
-      console.log('a');
       root.walkAtRules(rule => dealAtRule(rule, atRuleName));
       root.walkDecls(rule => dealDecl(rule, atRuleName));
       root.walkComments(comment => {
         comment.parent.removeChild(comment);
       });
       root.walkRules(rule => {
+        console.log('rule:', rule);
         if (rule.nodes.length === 0) {
           let parent = rule.parent;
           rule.parent.removeChild(rule);
           // 删除全部空树将会导致缺少一个分号
-          // while(parent) {
-          //   if(parent.nodes.length === 0) {
-          //     const temp = parent;
-          //     parent = parent.parent;
-          //     temp.remove();
-          //   } else {
-          //     break;
-          //   }
-          // }
+          while(parent) {
+            if(parent.nodes.length === 0) {
+              if (parent.parent) {
+                parent.parent.removeChild(parent);
+              } else {
+                parent.remove();
+              }
+              parent = parent.parent;
+            } else {
+              break;
+            }
+          }
         }
       });
-      console.log('b', atRuleName.size);
     }
-    if (root.nodes.length && atRuleName.size) {
-      return `/**THEMECSSBEGIN**\r\n${root.toString()}\r\n**THEMECSSEND**/\r\n${source}`;
+    root.nodes.forEach(deleteEmpty);
+    if (root.nodes.length || atRuleName.size) {
+      storage.add(root.toString(stringify));
+      // storage.add(root.toResult());
+      // sourceTemp = `/**THEMECSSBEGIN**\r\n${root.toString()}\r\n**THEMECSSEND**/\r\n${source}`;
     }
-    return source;
+    callback(null, sourceTemp);
+  });
+}
+function deleteEmpty(item) {
+  console.log('deleteEmpty:', item, item.type);
+  if(item.nodes && item.nodes.lenght) {
+    item.nodes.forEach(deleteEmpty)
+  } else {
+    item.remove();
+  }
 }
 function dealAtRule(rule, atRuleName) {
   const { params, name } = rule;
   let remove = true;
+  console.log('atrule:', params, name);
   globalVarsSet.forEach(key => {
-    if (params.indexOf(key) !== -1) {
+    const keyTemp = key.replace(/^@/, '');
+    if (params.indexOf(keyTemp) !== -1 || name.indexOf(keyTemp) !== -1) {
       remove = false;
-      atRuleName.add(name.replace(/:$/, ""));
+      atRuleName.add(name);
+    }
+  });
+  atRuleName.forEach(key => {
+    const keyTemp = key.replace(/^@/, '');
+    if (params.indexOf(keyTemp) !== -1 || name.indexOf(keyTemp) !== -1) {
+      remove = false;
     }
   });
   if (remove) {
-    rule.parent.removeChild(rule);
+    rule.remove();
   }
 }
 function dealDecl(rule, atRuleName) {
   const { value, prop } = rule;
+  console.log('dealDecl:', value, prop);
   let remove = true;
   globalVarsSet.forEach(key => {
-    if (value.indexOf(key) !== -1) {
+    const keyTemp = key.replace(/^@/, '');
+    if (value.indexOf(keyTemp) !== -1) {
       remove = false;
     }
   });
   atRuleName.forEach(key => {
-    if (value.indexOf(key) !== -1) {
+    const keyTemp = key.replace(/^@/, '');
+    if (value.indexOf(keyTemp) !== -1) {
       remove = false;
     }
   });
   if (remove || prop.startsWith('//')) {
-    rule.parent.removeChild(rule);
+    rule.remove();
   }
 }
 module.exports = themeLoader;
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 module.exports = {
   '@base-color': '#354',
-'@bg-color': '#555'
+'@bg-color': '#555',
+  '@base-font-color': '#fff'
 };
